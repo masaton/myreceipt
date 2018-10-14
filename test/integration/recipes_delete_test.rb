5d39c2cf6a59b17903260d5596e3b590525274b2
Masato Noguchi <masatonnoguchi@gmail.com>
2018-10-13 23:27:24 -0400

    Complete CRUD

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @chef = Chef.create!(chefname: "masato", email: "masatonnoguchi@gmail.com")
+    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+  end
+  
+  test "successfully delete a recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference "Recipe.count", -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
+  end
+  
+
+  
+  # test "the truth" do
+  #   assert true
+  # end
+end
